23f9fd4b252cc37bf5678f275f27084095080230
Vikas Dhiman <62803759+vikasdhiman0635@users.noreply.github.com>
2020-08-18 21:28:16 +0530

    Update cloudant.service.ts

--- frontend/Affine-Bank/src/app/Service/cloudant.service.ts
+++ frontend/Affine-Bank/src/app/Service/cloudant.service.ts
@@ -7,15 +7,30 @@ import { throwError, Observable } from 'rxjs';
 })
 export class CloudantService {
   
-
   constructor(private http: HttpClient){}
 
   createDoc(doc): Observable<{}> 
   {
-    console.log(doc);
     const url = "http://localhost:3000/insertto";
     return this.http.post<{}>(url, doc)
   }
 
-    
+  getalldatabycloudent():Observable<any>
+  {
+    const url = "http://localhost:3000/api/resource";
+    return this.http.get(url);
+  }
+  
+  deletedata(data: any):Observable<any>
+  {
+    const url = "http://localhost:3000/api/resource/"+data;
+    return this.http.delete(url);
+  }
+
+  updatedata(value: any):Observable<any>
+  {
+    const url = "http://localhost:3000/api/resource/";
+    return this.http.put(url,value);
+  }
+  
 }
