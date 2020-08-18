2e8c5531b1329c3a2f86d6eba8db5bc6da8a6b83
Vikas Dhiman <62803759+vikasdhiman0635@users.noreply.github.com>
2020-08-18 21:27:33 +0530

    Update app-routing.module.ts

--- frontend/Affine-Bank/src/app/app-routing.module.ts
+++ frontend/Affine-Bank/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ContectCurdComponent } from './contect-curd/contect-curd.component';
+import { UpdateDataComponent } from './contect-curd/update-data/update-data.component';
 
 
 const routes: Routes = [
@@ -9,6 +11,12 @@ const routes: Routes = [
   },
   {
     path:'Home', component:HomeComponent
+  },
+  {
+    path:'curd',component:ContectCurdComponent
+  },
+  {
+    path:'updatedata',component:UpdateDataComponent
   }
 ];
 
